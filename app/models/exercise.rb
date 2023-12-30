e54bdfd0fa9fa6afd9d9caf6a3c2d6827d165afd
Alexey Arbuzov <91293alex@gmail.com>
2023-12-30 21:10:30 +0400

    fix errors in active admin after new tables has been added

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -4,4 +4,9 @@ class Exercise < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     %w[created_at description id image_url name updated_at video_url]
   end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["exercise_registers"]
+  end
+
 end
--- app/models/exercise_register.rb
+++ app/models/exercise_register.rb
@@ -1,4 +1,9 @@
 class ExerciseRegister < ApplicationRecord
   belongs_to :exercise_set
   belongs_to :exercise
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[created_at exercise_id exercise_set_id id load reps_amount sets_amount updated_at]
+  end
+
 end
