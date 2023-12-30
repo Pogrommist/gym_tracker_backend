3b555e7e67f6749f93941b055029ea78ad66f949
Alexey Arbuzov <91293alex@gmail.com>
2023-12-30 20:35:25 +0400

    Create exercise_sets table

---
+++ db/migrate/20231230163253_create_exercise_sets.rb
@@ -0,0 +1,10 @@
+class CreateExerciseSets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :exercise_sets do |t|
+      t.string :name
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
