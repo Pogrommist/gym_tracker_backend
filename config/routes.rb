3b94620ea46c7bc8b86368abcb37e62fbc78484f
Alexey Arbuzov <91293alex@gmail.com>
2023-12-30 16:37:20 +0400

    Add acrtive admin gem and exercises to active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   mount_devise_token_auth_for 'User', at: 'auth'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
